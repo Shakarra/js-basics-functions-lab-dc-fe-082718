83dee80b1493132c8ce5b0c3245d215ba954614f
Shakarra McGuire <shakarramcguire@gmail.com>
2018-10-16 00:15:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+//let feet = 43;
+distanceFromHqInBlocks(43) {
+  return 1;
+}
+
+// distanceFromHqInFeet() {}
+
+distanceTravelledInFeet(50,42) {
+  (50-42) * 264 = 2112
+}
+
+// calculatesFarePrice() {}
